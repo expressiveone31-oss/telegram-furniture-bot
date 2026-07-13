b4fc6c566c39c5a4f67168c361684aedbc06c0e7
expressiveone31-oss <expressive.one31@gmail.com>
2026-07-13 18:31:31 +0300

    Rename main menu buttons

--- app/keyboards/main.py
+++ app/keyboards/main.py
@@ -7,8 +7,8 @@ from app.texts import BACK_TO_CATALOG, BACK_TO_MENU
 def get_main_menu_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text="📚 Посмотреть каталог", callback_data="catalog"),
-        InlineKeyboardButton(text="📦 Как оформить заказ", callback_data="how_it_works"),
+        InlineKeyboardButton(text="Каталог 🤍", callback_data="catalog"),
+        InlineKeyboardButton(text="Про процесс 📦", callback_data="how_it_works"),
         InlineKeyboardButton(text="❓ Задать вопрос", callback_data="ask_question"),
     )
     return builder.as_markup()
